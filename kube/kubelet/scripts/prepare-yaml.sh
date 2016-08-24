90e48fbc034620bfb997cca683ab685d3a925584
Cedric Hauber <cedric.hauber@wescale.fr>
2016-08-24 12:16:38 +0200

    Kubelet docker

--- kube/kubelet/scripts/prepare-yaml.sh
+++ kube/kubelet/scripts/prepare-yaml.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 if [[ "${MASTER}" == "true" ]]
 then
-  cp /opt/manifests/master/* /etc/kubernetes/manifests
+  cp /opt/kube/manifests/master/* /etc/kubernetes/manifests
 else
-  cp /opt/manifests/node/* /etc/kubernetes/manifests
+  cp /opt/kube/manifests/node/* /etc/kubernetes/manifests
 fi
-cp /opt/descriptors/* /etc/kubernetes/descriptors
+cp /opt/kube/descriptors/* /etc/kubernetes/descriptors
 for file in $(ls /etc/kubernetes/descriptors/*.yaml /etc/kubernetes/manifests/*.yaml)
 do
     sed -i s/\\\$domain\\\$/${DOMAIN}/g $file
