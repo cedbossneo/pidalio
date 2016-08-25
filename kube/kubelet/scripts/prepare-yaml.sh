e4f406ca9a81ec7449cc5e0f959b89101a1d7f90
Cedric Hauber <cedric.hauber@wescale.fr>
2016-08-25 15:11:35 +0200

    Kubelet docker

--- kube/kubelet/scripts/prepare-yaml.sh
+++ kube/kubelet/scripts/prepare-yaml.sh
@@ -7,13 +7,13 @@ else
 
   echo "Waiting for Kubernetes..."
   PIDALIO_URL=http://$(/opt/bin/weave dns-lookup pidalio):3000
-  until [[ "curl --write-out '%{http_code}' --silent --output /dev/null $PIDALIO_URL/k8s/masters\?token\=${PIDALIO_TOKEN}" == "200" ]]
+  until [[ "$(curl --write-out '%{http_code}' --silent --output /dev/null $PIDALIO_URL/k8s/masters\?token\=$PIDALIO_TOKEN)" == "200" ]]
   do
     echo "Trying: $PIDALIO_URL/k8s/masters"
     sleep 10
   done
-  export MASTERS_URLS=$(curl -s $PIDALIO_URL/k8s/masters\?token\=${PIDALIO_TOKEN} | jq -r .urls | tr '\n' ',')
-  export MASTER_URL=$(curl -s $PIDALIO_URL/k8s/masters\?token\=${PIDALIO_TOKEN} | jq -r .urls | head -n 1)
+  export MASTERS_URLS=$(curl -s $PIDALIO_URL/k8s/masters\?token\=${PIDALIO_TOKEN} | jq -r .urls[] | tr '\n' ',')
+  export MASTER_URL=$(curl -s $PIDALIO_URL/k8s/masters\?token\=${PIDALIO_TOKEN} | jq -r .urls[] | head -n 1)
 fi
 cp /opt/kube/descriptors/* /etc/kubernetes/descriptors
 for file in $(ls /etc/kubernetes/descriptors/*.yaml /etc/kubernetes/manifests/*.yaml)
