ef928d6b8a26d5348632c839ab2a74479b8e3720
Cedric Hauber <cedric.hauber@wescale.fr>
2016-08-29 15:30:59 +0200

    Kubelet docker

--- kube/kubelet/scripts/launch-kubelet.sh
+++ kube/kubelet/scripts/launch-kubelet.sh
@@ -8,9 +8,12 @@ then
     --register-node=true \
     --allow-privileged=true \
     --config=/etc/kubernetes/manifests \
-    --hostname-override=${NODE_IP} \
+    --node-ip=${NODE_IP} \
+    --hostname-override=${NODE_NAME} \
     --cluster-dns=10.16.0.3 \
     --cluster-domain=${DOMAIN} \
+    --cloud-provider=openstack \
+    --cloud-config=/etc/kubernetes/cloud.conf \
     --tls-cert-file=/etc/kubernetes/ssl/node.pem \
     --tls-private-key-file=/etc/kubernetes/ssl/node-key.pem \
     --kubeconfig=/etc/kubernetes/kubeconfig.yaml \
@@ -19,19 +22,15 @@ else
   PIDALIO_URL=http://$(/opt/bin/weave dns-lookup pidalio):3000
   MASTERS_URLS=$(curl -s ${PIDALIO_URL}/k8s/masters\?token\=${PIDALIO_TOKEN} | jq -r .urls[] | tr '\n' ',')
   echo Masters: ${MASTERS_URLS}
-  until [[ "$(curl --write-out '%{http_code}' --silent --output /dev/null -XPOST $PIDALIO_URL/register/node\?token\=$PIDALIO_TOKEN\&id=$NODE_ID\&ip=$NODE_IP\&os=linux\&arch=amd64)" == "200" ]]
-  do
-        echo "Trying to register node"
-        sleep 10
-  done
   /opt/bin/kubelet \
     --docker-endpoint=unix:///var/run/weave/weave.sock \
     --api-servers=${MASTERS_URLS} \
-    --register-node=false \
+    --register-node=true \
     --node-labels=mode=SchedulingDisabled \
     --allow-privileged=true \
     --config=/etc/kubernetes/manifests \
-    --hostname-override=${NODE_IP} \
+    --node-ip=${NODE_IP} \
+    --hostname-override=${NODE_NAME} \
     --cluster-dns=10.16.0.3 \
     --cluster-domain=${DOMAIN} \
     --tls-cert-file=/etc/kubernetes/ssl/node.pem \
