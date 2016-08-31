a59812a762e89ed76d27ebe0ad6adce486a919b2
Cedric Hauber <cedric.hauber@wescale.fr>
2016-08-31 17:52:50 +0200

    Kubelet docker

--- init.sh
+++ init.sh
@@ -1,46 +1,5 @@
 #!/usr/bin/env bash
 set -xe
-source /etc/pidalio.env
-# Configure ETCD
-if cat /etc/etcd.env | grep ETCD_NAME; then exit 0; fi
-if [[ "$PEERS" == "$NODE_IP" ]]
-then
-    cat <<EOF > /etc/etcd.env
-ETCD_ADVERTISE_CLIENT_URLS=http://${NODE_IP}:2379,http://${NODE_PUBLIC_IP}:2379
-ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${NODE_IP}:2380,http://${NODE_PUBLIC_IP}:2380
-ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379
-ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380
-ETCD_NAME=${NODE_FQDN}
-ETCD_INITIAL_CLUSTER=${NODE_FQDN}=http://${NODE_IP}:2380,${NODE_FQDN}=http://${NODE_PUBLIC_IP}:2380
-ETCD_INITIAL_CLUSTER_STATE=new
-EOF
-else
-    ETCD_PEERS=""
-    for server in ${PEERS}
-    do
-      ETCD_PEERS=http://${server}:2379,${ETCD_PEERS}
-    done
-    ETCD_PEERS=$(echo ${ETCD_PEERS}|sed -rn 's/^(.*),$/\1/p')
-    echo "EtcD peers: $ETCD_PEERS"
-    until etcdctl --no-sync --endpoints ${ETCD_PEERS} member list >/dev/null 2>&1; do
-        echo "Waiting for EtcD at $ETCD_PEERS..."
-        sleep 10
-    done
-    until cat /tmp/etcd.env | grep ETCD_NAME
-    do
-        echo "Trying to register Etcd node"
-        etcdctl --endpoints ${ETCD_PEERS} member add ${NODE_FQDN} http://${NODE_PUBLIC_IP}:2380 | tail -n +3 > /tmp/etcd.env
-        sleep 10
-    done
-    mv /tmp/etcd.env /etc/etcd.env
-    cat <<EOF >> /etc/etcd.env
-ETCD_ADVERTISE_CLIENT_URLS=http://${NODE_IP}:2379,http://${NODE_PUBLIC_IP}:2379
-ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${NODE_IP}:2380,http://${NODE_PUBLIC_IP}:2380
-ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379
-ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380
-EOF
-fi
-
 # Create directories and download Kubernetes Components
 mkdir -p /etc/kubernetes/descriptors /etc/kubernetes/manifests /etc/kubernetes/ssl /opt/bin
 if [[ -x /opt/bin/kubelet ]]; then
@@ -55,3 +14,30 @@ else
     curl -o /opt/bin/kubectl http://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kubectl
     chmod +x /opt/bin/kubectl
 fi
+if [[ -x /opt/bin/etcd2-bootstrapper ]]; then
+    echo "Etcd2 Bootstrapper already installed"
+else
+    curl -o /opt/bin/etcd2-bootstrapper https://github.com/glerchundi/etcd2-bootstrapper/releases/download/v0.3.0/etcd2-bootstrapper-linux-amd64
+    chmod +x /opt/bin/etcd2-bootstrapper
+fi
+source /etc/pidalio.env
+# Configure ETCD
+if cat /etc/etcd.env | grep ETCD_NAME; then exit 0; fi
+ETCD_PEERS=""
+for server in ${PEERS}
+do
+  ETCD_PEERS=${server}=${server},${ETCD_PEERS}
+done
+ETCD_PEERS=$(echo ${ETCD_PEERS}|sed -rn 's/^(.*),$/\1/p')
+echo "EtcD peers: $ETCD_PEERS"
+until etcd2-bootstrapper --me ${NODE_IP}=${NODE_IP} --members ${ETCD_PEERS} --out /etc/etcd.env
+do
+    echo "Trying to register Etcd node"
+    sleep 10
+done
+cat <<EOF >> /etc/etcd.env
+ETCD_ADVERTISE_CLIENT_URLS=http://${NODE_IP}:2379,http://${NODE_PUBLIC_IP}:2379
+ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${NODE_IP}:2380,http://${NODE_PUBLIC_IP}:2380
+ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379
+ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380
+EOF
