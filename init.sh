d756f0fcfbdf0d076ea0a9c824231a7ce4f8c9d7
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-01 17:03:13 +0200

    Fix node count

--- init.sh
+++ init.sh
@@ -14,44 +14,7 @@ else
     curl -o /opt/bin/kubectl http://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kubectl
     chmod +x /opt/bin/kubectl
 fi
-if [[ -x /opt/bin/etcd2-bootstrapper ]]; then
-    echo "Etcd2 Bootstrapper already installed"
-else
-    wget -O /opt/bin/etcd2-bootstrapper https://github.com/glerchundi/etcd2-bootstrapper/releases/download/v0.3.0/etcd2-bootstrapper-linux-amd64
-    chmod +x /opt/bin/etcd2-bootstrapper
-fi
 source /etc/pidalio.env
-# Configure ETCD
-if [[ "$PEER" == "$NODE_IP" ]]
-then
-    MEMBERS="$NODE_FQDN=$NODE_PUBLIC_IP"
-else
-    until curl -s http://$PEER:2380/members
-    do
-        echo "Trying to find members"
-        sleep 10
-    done
-    MEMBERS=$(curl -s http://$PEER:2380/members | jq -r '.[] | "\(.name)=\(.peerURLs[0])"')
-fi
-ETCD_PEERS=""
-for MEMBER in ${MEMBERS}
-do
-  ETCD_PEERS=${MEMBER},${ETCD_PEERS}
-done
-ETCD_PEERS=$(echo ${ETCD_PEERS}|sed -rn 's/^(.*),$/\1/p' | sed 's/http:\/\///g' | sed 's/:2380//g')
-
-echo "EtcD peers: $ETCD_PEERS"
-until /opt/bin/etcd2-bootstrapper --me ${NODE_FQDN}=${NODE_PUBLIC_IP} --members ${ETCD_PEERS} --out /etc/etcd.env
-do
-    echo "Trying to register Etcd node"
-    sleep 10
-done
-. /etc/etcd.env
-/usr/bin/etcd2 \
-    -advertise-client-urls=http://${NODE_PUBLIC_IP}:2379 \
-    -initial-advertise-peer-urls=http://${NODE_PUBLIC_IP}:2380 \
-    -listen-client-urls=http://0.0.0.0:2379 \
-    -listen-peer-urls=http://0.0.0.0:2380 \
-    -initial-cluster=${ETCD_INITIAL_CLUSTER} \
-    -name=${ETCD_NAME} \
-    -initial-cluster-state=${ETCD_INITIAL_CLUSTER_STATE}
+# Launch ETCD
+export DOCKER_HOST=unix:///var/run/weave/weave.sock
+docker run -p 2379:2379 -p 2380:2380 cedbossneo/etcd-cluster-on-docker
