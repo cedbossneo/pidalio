91c89bfab194d07a8695740510cd54fea1277810
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-01 18:28:05 +0200

    Fix node count

--- init.sh
+++ init.sh
@@ -18,4 +18,4 @@ source /etc/pidalio.env
 # Launch ETCD
 export DOCKER_HOST=unix:///var/run/weave/weave.sock
 docker pull cedbossneo/etcd-cluster-on-docker
-docker run -e AUTHORITATIVE_ID=${PEER} -e ID=${NODE_IP} -e FS_PATH=/var/etcd --rm --name=etcd -e NODE_COUNT=3 -p 2379:2379 -p 2380:2380 -p 4001:4001 -p 7001:7001 cedbossneo/etcd-cluster-on-docker
+docker run -e FS_PATH=/var/etcd -v /var/etcd:/opt/etcd  --rm --name=etcd -e NODE_COUNT=3 -p 2379:2379 -p 2380:2380 -p 4001:4001 -p 7001:7001 cedbossneo/etcd-cluster-on-docker
