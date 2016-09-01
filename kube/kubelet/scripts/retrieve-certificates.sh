5f9dcbd12bac21a137ce1b205ab85e121403e31b
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-01 13:43:26 +0200

    Fix node count

--- kube/kubelet/scripts/retrieve-certificates.sh
+++ kube/kubelet/scripts/retrieve-certificates.sh
@@ -14,7 +14,7 @@ done
 # Root CA
 curl -s ${PIDALIO_URL}/certs/ca\?token\=${PIDALIO_TOKEN} > ca.json
 cat ca.json | jq -r .cert > /etc/kubernetes/ssl/ca.pem
-curl -s ${PIDALIO_URL}/certs/node\?token\=${PIDALIO_TOKEN}\&fqdn=${NODE_FQDN}\&ip=${NODE_IP} > node.json
+curl -s ${PIDALIO_URL}/certs/node\?token\=${PIDALIO_TOKEN}\&fqdn=${NODE_FQDN}\&ip=${NODE_PUBLIC_IP} > node.json
 cat node.json | jq -r .privateKey > /etc/kubernetes/ssl/node-key.pem
 cat node.json | jq -r .cert > /etc/kubernetes/ssl/node.pem
 cat <<EOF > /etc/kubernetes/kubeconfig.yaml
@@ -39,7 +39,7 @@ EOF
 if [[ "${MASTER}" == "true" ]]
 then
   # Server Certificate
-  curl -s ${PIDALIO_URL}/certs/server\?token\=${PIDALIO_TOKEN}\&ip=${NODE_IP} > server.json
+  curl -s ${PIDALIO_URL}/certs/server\?token\=${PIDALIO_TOKEN}\&ip=${NODE_PUBLIC_IP} > server.json
   cat server.json | jq -r .privateKey > /etc/kubernetes/ssl/server-key.pem
   cat server.json | jq -r .cert > /etc/kubernetes/ssl/server.pem
 fi
