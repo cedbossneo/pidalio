ab5f40776fe80de688191dd30585daf4dc64387e
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-22 11:26:58 +0200

    Fleet approach

---
+++ kube/kubelet/scripts/prepare-units.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+for file in $(ls /opt/pidalio/kube/units/*.service /opt/pidalio/kube/units/master/*.service)
+do
+    sed -i s/\\\$region\\\$/${REGION}/g ${file}
+    sed -i s/\\\$token\\\$/${TOKEN}/g ${file}
+done
