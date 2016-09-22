39a927899b54280fb44745fd75a3d68300709419
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-22 13:55:38 +0200

    Restore TLS

---
+++ dockerfiles/apiserver/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [[ "${MASTER}" == "true" ]]
+then
+    if [ ! -e /etc/kubernetes/ssl/server.pem ]; then
+      MY_IP=$(ip -4 addr show scope global dev ethwe | grep inet | awk '{print $2}' | cut -d / -f 1)
+      curl -s http://pidalio:3000/certs/server\?token\=${PIDALIO_TOKEN}\&ip=${MY_IP} > server.json
+      cat server.json | jq -r .privateKey > /etc/kubernetes/ssl/server-key.pem
+      cat server.json | jq -r .cert > /etc/kubernetes/ssl/server.pem
+    fi
+fi
+exec "@*"
