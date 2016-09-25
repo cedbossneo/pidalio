b3bb1baedd1174fddcf444792e6d2c43f1c2401d
Cedric Hauber <cedric.hauber@wescale.fr>
2016-09-25 15:51:43 +0200

    Extract components download

--- init.sh
+++ init.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 set -e
-# Create directories and download Kubernetes Components
-mkdir -p /etc/kubernetes/descriptors /etc/kubernetes/manifests /etc/kubernetes/ssl /opt/bin
-for app in kubelet kube-proxy kubectl
-do
-    if [[ -x /opt/bin/$app ]]; then
-        echo "$app already installed"
-    else
-        curl -o /opt/bin/$app http://storage.googleapis.com/kubernetes-release/release/v1.3.7/bin/linux/amd64/$app
-        chmod +x /opt/bin/$app
-        echo "$app installed"
-    fi
-done
 source /etc/pidalio.env
+/opt/pidalio/kube/kubelet/scripts/download-components.sh
 /opt/pidalio/kube/kubelet/scripts/prepare-units.sh
 #/opt/pidalio/kube/kubelet/scripts/ceph/install-ceph-tools.sh
 docker pull cedbossneo/etcd-cluster-on-docker
---
+++ kube/kubelet/scripts/download-components.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Create directories and download Kubernetes Components
+mkdir -p /etc/kubernetes/descriptors /etc/kubernetes/manifests /etc/kubernetes/ssl /opt/bin
+for app in kubelet kube-proxy kubectl
+do
+    if [[ -x /opt/bin/$app ]]; then
+        echo "$app already installed"
+    else
+        curl -o /opt/bin/$app http://storage.googleapis.com/kubernetes-release/release/v1.3.7/bin/linux/amd64/$app
+        chmod +x /opt/bin/$app
+        echo "$app installed"
+    fi
+done
