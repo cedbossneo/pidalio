de82083a7c7f29cec55d9753043dcaff989260d9
Cedric Hauber <cedric.hauber@wescale.fr>
2016-10-08 15:10:13 +0200

    Fix rm

--- kube/kubelet/scripts/prepare-yaml.sh
+++ kube/kubelet/scripts/prepare-yaml.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -f /etc/kubernetes/descriptors/*
+rm -Rf /etc/kubernetes/descriptors/*
 cp -Rf /opt/pidalio/kube/kubelet/descriptors/* /etc/kubernetes/descriptors
 for file in $(ls /etc/kubernetes/descriptors/dns/*.yaml)
 do
