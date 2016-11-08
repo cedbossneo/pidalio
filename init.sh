4b0b09c15fec5589b36e1fbc4e663cd0daaa93b7
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-08 14:34:25 +0100

    New Etcd mechanism

--- init.sh
+++ init.sh
@@ -8,45 +8,25 @@ then
     /opt/pidalio/kube/kubelet/scripts/ceph/install-ceph-tools.sh
     docker pull ceph/base
 fi
-docker pull cedbossneo/etcd-cluster-on-docker
+docker pull cedbossneo/docker-etcd-rclone
 export DOCKER_HOST=unix:///var/run/weave/weave.sock
 SLEEP_TIME=$(expr $RANDOM % 50)
 echo "Sleeping $SLEEP_TIME seconds"
 sleep ${SLEEP_TIME}
-EXISTING_IPS=$(/opt/bin/weave dns-lookup etcd | sort)
-EXISTING_IDS=""
-echo "Existing IPS: $EXISTING_IPS"
-for ip in ${EXISTING_IPS}
-do
-    echo "Testing $ip"
-    curl -s -m 1 http://${ip}:2379/v2/stats/self
+EXISTING_ETCD=$(/opt/bin/weave dns-lookup etcd)
+echo "Existing ETCD: $EXISTING_ETCD"
+if [[ "$EXISTING_ETCD" == "" ]]
+then
+    docker run -e TOKEN=${TOKEN} -e OS_USERNAME=${OS_USERNAME} -e OS_PASSWORD=${OS_PASSWORD} -e OS_AUTH_URL=${OS_AUTH_URL} -e OS_TENANT_NAME=${OS_TENANT_NAME} -e OS_REGION_NAME=${OS_REGION_NAME} --rm --name=etcd -p 2379:2379 -p 2380:2380 cedbossneo/docker-etcd-rclone
+else
+    echo "Testing $EXISTING_ETCD"
+    curl -s -m 1 http://${EXISTING_ETCD}:2379/v2/stats/self
     if [ $? -eq 0 ]
     then
-        IP_ID=$(curl -s -m 10 http://${ip}:2379/v2/stats/self | jq -r .name | cut -d'-' -f 2)
-        EXISTING_IDS=${IP_ID},${EXISTING_IDS}
-        echo "Etcd $ip already exist, ID: $IP_ID";
+        echo "Etcd $EXISTING_ETCD alive";
     else
-        echo "Etcd $ip did not come up...exiting"
+        echo "Etcd $EXISTING_ETCD did not come up...exiting"
         exit 1
     fi
-done
-ID="-1"
-MAX=$(expr ${ETCD_NODES} - 1)
-for id in $(seq 0 ${MAX})
-do
-    if [[ "$EXISTING_IDS" == *"$id"* ]]
-    then
-        echo "ID $id already taken";
-    else
-        ID="${id}"
-        break;
-    fi
-done
-if [ "$ID" -eq "-1" ]
-then
-    echo "Running as proxy"
-    docker run --rm --name=etcd-proxy -p 2379:2379 -p 2380:2380 cedbossneo/etcd-cluster-on-docker /bin/etcd_proxy.sh
-else
-    echo "Using ID: $ID"
-    docker run -e ID=${ID} -e FS_PATH=/var/etcd --rm --name=etcd -p 2379:2379 -p 2380:2380 cedbossneo/etcd-cluster-on-docker
+    docker run --rm --name=etcd-proxy -p 2379:2379 -p 2380:2380 cedbossneo/docker-etcd-rclone etcd -proxy on -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -initial-cluster etcd=http://${EXISTING_ETCD}:2380
 fi
