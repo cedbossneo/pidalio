e6c0005f9ad082c3297196a9eadd98cc1a899983
Cedric Hauber <cedric.hauber@wescale.fr>
2016-11-16 13:23:44 +0100

    Fix ceph disk

--- kube/kubelet/scripts/prepare-yaml.sh
+++ kube/kubelet/scripts/prepare-yaml.sh
@@ -5,5 +5,5 @@ for file in $(ls /etc/kubernetes/descriptors/dns/*.yaml /etc/kubernetes/descript
 do
     sed -i s/\\\$domain\\\$/${DOMAIN}/g ${file}
     sed -i s/\\\$node_type\\\$/${NODE_TYPE}/g ${file}
-    sed -i s/\\\$CEPH_DISK_DEVICE\\\$/${CEPH_DISK_DEVICE}/g ${file}
+    sed -i s#\\\$CEPH_DISK_DEVICE\\\$#${CEPH_DISK_DEVICE}#g ${file}
 done
