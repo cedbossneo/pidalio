e9ab2450c140a7f3e851c1225475d4c5ce1a0527
Cedric Hauber <cedric.hauber@wescale.fr>
2017-01-11 12:12:55 +0100

    Support for provider specific dns

--- kube/kubelet/scripts/configure-dns.sh
+++ kube/kubelet/scripts/configure-dns.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source /etc/pidalio.env
 echo "Setting DNS"
 WEAVE_DNS_ADDRESS=$(/opt/bin/weave report | jq -r .DNS.Address | cut -d ':' -f 1)
 until [[ "$WEAVE_DNS_ADDRESS" != "" ]];
@@ -8,7 +9,7 @@ do
 done
 cat <<EOF > /opt/resolv.conf
 nameserver ${WEAVE_DNS_ADDRESS}
-nameserver 185.23.94.244
+nameserver ${PROVIDER_DNS}
 EOF
 rm -f /etc/resolv.conf
 ln -s /opt/resolv.conf /etc/resolv.conf
