95a1a64fcf2b5a12d00123b71d59a7f813e7c5aa
Nicolas Bernard <nikkau@nikkau.net>
2017-04-03 20:20:01 +0200

    Replace Gin by Chi

--- api/Server.go
+++ api/Server.go
@@ -3,74 +3,66 @@ package api
 import (
 	"errors"
 	"github.com/cedbossneo/pidalio/ssl"
-	"github.com/gin-gonic/gin"
+	"github.com/pressly/chi"
 	"net/http"
 )
 
-func checkErrors(c *gin.Context, err error) bool {
-	if err != nil {
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return true
+func mid(rootCerts ssl.RootCerts) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Query().Get("token")[0:16] != rootCerts.Token {
+				http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
 	}
-	return false
 }
 
-func CreateAPIServer(rootCerts ssl.RootCerts, serverCerts ssl.ServerCerts, bindAddress string) {
-	r := gin.Default()
-	r.Use(func(c *gin.Context) {
-		if c.Query("token")[0:16] != rootCerts.Token {
-			c.AbortWithStatus(http.StatusForbidden)
-		} else {
-			c.Next()
-		}
+func CreateAPIServer(rootCerts ssl.RootCerts, serverCerts ssl.ServerCerts) (http.Handler, error) {
+	r := chi.NewRouter()
+	r.Use(mid(rootCerts))
+
+	if cert, err := rootCerts.Certificate.MarshalPEM(); err != nil {
+		return nil, err
+	}
+	resCert := fmt.Sprintf(`{"cert": "%s"}`, cert)
+	r.Get("/certs/ca", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(resCert))
 	})
-	r.GET("/certs/ca", func(c *gin.Context) {
-		cert, err := rootCerts.Certificate.MarshalPEM()
-		if checkErrors(c, err) {
+
+	if cert, private, public, err := ssl.CreateAdminCertificate(rootCerts); err != nil {
+		return nil, err
+	}
+	resAdmin := fmt.Sprintf(`{"cert": "%s", "privateKey": "%s", "publicKey": "%s"}`, cert, private, public)
+	r.Get("/certs/admin", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(resCert))
+	})
+
+	resServer := fmt.Sprintf(`{"cert": "%s", "privateKey": "%s", "publicKey": "%s"}`, serverCerts.Certificate, serverCerts.PrivateKey, serverCerts.PublicKey)
+	r.Get("/certs/server", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(resServer))
+	})
+
+	r.Get("/certs/node", func(w http.ResponseWriter, r *http.Request) {
+		ip := r.URL.Query().Get("ip")
+		if ip == "" {
+			http.Error(w, "ip not defined", http.StatusBadRequest)
 			return
 		}
-		c.JSON(200, gin.H{
-			"cert": string(cert),
-		})
-	})
-	r.GET("/certs/admin", func(c *gin.Context) {
-		cert, private, public, err := ssl.CreateAdminCertificate(rootCerts)
-		if checkErrors(c, err) {
+
+		fqdn := r.URL.Query().Get("fqdn")
+		if fqdn == "" {
+			http.Error(w, "fqdn not defined", http.StatusBadRequest)
 			return
 		}
-		c.JSON(200, gin.H{
-			"cert":       string(cert),
-			"privateKey": string(private),
-			"publicKey":  string(public),
-		})
-	})
-	r.GET("/certs/server", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"cert":       string(serverCerts.Certificate),
-			"privateKey": string(serverCerts.PrivateKey),
-			"publicKey":  string(serverCerts.PublicKey),
-		})
-	})
-	r.GET("/certs/node", func(c *gin.Context) {
-		ip, exist := c.GetQuery("ip")
-		if !exist {
-			c.AbortWithError(http.StatusBadRequest, errors.New("ip not defined"))
+
+		if cert, private, public, err := ssl.CreateNodeCertificate(rootCerts, fqdn, ip); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		fqdn, exist := c.GetQuery("fqdn")
-		if !exist {
-			c.AbortWithError(http.StatusBadRequest, errors.New("fqdn not defined"))
-			return
-		}
-		cert, private, public, err := ssl.CreateNodeCertificate(rootCerts, fqdn, ip)
-		if checkErrors(c, err) {
-			return
-		}
-		c.JSON(200, gin.H{
-			"cert":       string(cert),
-			"privateKey": string(private),
-			"publicKey":  string(public),
-		})
+		res := fmt.Sprintf(`{"cert": "%s", "privateKey": "%s", "publicKey": "%s"}`, cert, private, public)
+		w.Write([]byte(res))
 	})
-	r.Run(bindAddress)
+	return r, nil
 }
